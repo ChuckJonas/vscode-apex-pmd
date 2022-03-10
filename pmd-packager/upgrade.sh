fd8f0db5cf2d583d0e8a849ced1be8892f0f6e1a
David Schach <dschach@x2od.com>
2022-03-09 17:08:34 -0800

    Auto-pull latest version number
    This is the first step in automating updating to the latest PMD version and automating package creation when there's a new version.
    
    Remove newline from echo
    
    Co-Authored-By: Andreas Dangel <andreas.dangel@adangel.org>

--- pmd-packager/upgrade.sh
+++ pmd-packager/upgrade.sh
@@ -1,5 +1,7 @@
-VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".tag_name" | sed 's:.*/::')
+VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | grep '.tag_name' | sed 's:.*/::' | sed 's:",::')
 echo $VERSION
+echo -n $VERSION > pmd-packager/pmd-version.txt
+sed -i "" "s|version\>[0-9\.]*\<|version>$VERSION<|g" pmd-packager/pom.xml
 ./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Dpmd.version=$VERSION
 rm -rf ../bin/pmd/*
 bsdtar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
