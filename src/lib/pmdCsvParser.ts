b5bdadba9dc76605d2fd6397ebf3c8ca92846c97
David Schach <dschach@x2od.com>
2022-03-09 17:01:02 -0800

    Dependency upgrades

--- src/lib/pmdCsvParser.ts
+++ src/lib/pmdCsvParser.ts
@@ -1,5 +1,6 @@
-import * as parser from 'csv-parse/lib/sync';
-import { Options } from 'csv-parse';
+//import * as parser from 'csv-parse/lib/sync.js';
+//import { Options } from 'csv-parse';
+import { parse } from 'csv-parse/sync';
 import { EOL } from 'os';
 import * as vscode from 'vscode';
 
@@ -16,20 +17,20 @@ const PMD_COLUMNS: (keyof PmdResult)[] = [
 
 export function parsePmdCsv(csv: string): Array<PmdResult> {
   let results: PmdResult[];
-  const parseOpts: Options = {
+  const parseOpts = {
     columns: PMD_COLUMNS,
     // eslint-disable-next-line @typescript-eslint/camelcase
     relax_column_count: true,
   };
   try {
-    results = parser(csv, parseOpts);
+    results = parse(csv, parseOpts);
   } catch (e) {
     //try to recover parsing... remove last ln and try again
     const lines = csv.split(EOL);
     lines.pop();
     csv = lines.join(EOL);
     try {
-      results = parser(csv, parseOpts);
+      results = parse(csv, parseOpts);
     } catch (e) {
       throw new Error(
         'Failed to parse PMD Results.  Enable please logging (STDOUT & STDERROR) and submit an issue if this problem persists.'
