5a05dde235ab1910aa429247c91fb2f922afb668
David Schach <dschach@x2od.com>
2022-03-23 13:34:14 -0700

    Reference correct pom.xml location

--- pmd-packager/upgrade.sh
+++ pmd-packager/upgrade.sh
@@ -1,7 +1,7 @@
 VERSION="$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | sed -n '/"tag_name"/ s|.*\([0-9]\+\.[0-9]\+\.[0-9]\+\).*|\1| p')"
 echo $VERSION
 echo -n $VERSION > pmd-version.txt
-sed -i "" "s|version\>[0-9\.]*\<|version>$VERSION<|g" pmd-packager/pom.xml
+sed -i "" "s|version\>[0-9\.]*\<|version>$VERSION<|g" pom.xml
 ./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Dpmd.version=$VERSION
 rm -rf ../bin/pmd/*
 bsdtar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
