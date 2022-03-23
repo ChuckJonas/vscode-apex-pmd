c44c259df2a0568a201f3d6bb597c66909fedbed
David Schach <dschach@x2od.com>
2022-03-23 16:42:24 -0700

    Scripts update

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-rm -rf ${DIR}/pmd/*
-VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".tag_name")
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".assets[].browser_download_url" | grep pmd-bin)
+echo $DIR
+
+VERSION=$(<${DIR}/pmd-packager/pmd-version.txt)
+
+DOWNLOAD_URL="https://github.com/pmd/pmd/releases/download/pmd_releases%2F$VERSION/pmd-bin-$VERSION.zip"
 echo "version=${VERSION}"
 echo "url=${DOWNLOAD_URL}"
-curl $DOWNLOAD_URL -L -o ${DIR}/pmd.zip
-bsdtar --strip-components=1 -xvf ${DIR}/pmd.zip -C ${DIR}/pmd
-rm ${DIR}/pmd.zip
+
+#./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Dpmd.version=$VERSION
+rm -rf ${DIR}/pmd/*
+#bsdtar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
+
+curl ${DOWNLOAD_URL} -L -o ../bin/pmd.zip
+bsdtar --strip-components=1 -xvf ../bin/pmd.zip -C ../bin/pmd
+rm ../bin/pmd.zip
---
+++ pmd-packager/latestversion.sh
@@ -0,0 +1,10 @@
+VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | grep '.tag_name' | sed 's:.*/::' | sed 's:",::')
+echo $VERSION
+printf '%s' "$VERSION" > pmd-version.txt
+sed -i "" "s|version\>[0-9\.]*\<|version>$VERSION<|g" pom.xml
+
+# Keep the following lines for when we allow the user to override the PMD version used or we force the latest version
+# DOWNLOAD_URL="https://github.com/pmd/pmd/releases/download/pmd_releases%2F$VERSION/pmd-bin-$VERSION.zip"
+# DOWNLOAD_URL=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | grep -m 1 '.browser_download_url' | sed 's:.*http:http:' | sed 's:",::' | sed 's:"::')
+# echo "url=${DOWNLOAD_URL}"
+
