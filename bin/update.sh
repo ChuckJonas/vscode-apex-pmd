6df2c51e9289ac1f2f08a0a44109875809d571db
David Schach <dschach@x2od.com>
2022-03-23 17:45:22 -0700

    Update scripts

--- bin/update.sh
+++ bin/update.sh
@@ -1,16 +1,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo $DIR
+echo "DIR={$DIR}"
 
-VERSION=$(<${DIR}/pmd-packager/pmd-version.txt)
+VERSION=$(<${DIR}/pmd-version.txt)
+echo "VERSION=${VERSION}"
 
 DOWNLOAD_URL="https://github.com/pmd/pmd/releases/download/pmd_releases%2F$VERSION/pmd-bin-$VERSION.zip"
-echo "version=${VERSION}"
-echo "url=${DOWNLOAD_URL}"
+echo "URL=${DOWNLOAD_URL}"
 
-#./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Dpmd.version=$VERSION
-rm -rf ${DIR}/pmd/*
-#bsdtar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
+# ./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Dpmd.version=$VERSION
+rm -rf ${DIR}/../bin/pmd/*
+# bsdtar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
 
-curl ${DOWNLOAD_URL} -L -o ../bin/pmd.zip
-bsdtar --strip-components=1 -xvf ../bin/pmd.zip -C ../bin/pmd
-rm ../bin/pmd.zip
+curl ${DOWNLOAD_URL} -L -o ${DIR}/../bin/pmd.zip
+bsdtar --strip-components=1 -xvf ${DIR}/../bin/pmd.zip -C ${DIR}/../bin/pmd
+rm ${DIR}/../bin/pmd.zip
