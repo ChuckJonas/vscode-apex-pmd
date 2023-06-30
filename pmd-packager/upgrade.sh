897283daeebf88c261bad0fbaa2d865eaec4eb64
Pablo Venturino <pablojventurino@gmail.com>
2023-06-29 23:19:30 -0700

    0.5.10 - upgrade to PMD 6.55.0

--- pmd-packager/upgrade.sh
+++ pmd-packager/upgrade.sh
@@ -1,5 +1,7 @@
 VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".tag_name" | sed 's:.*/::')
+VERSION="6.55.0"
 echo $VERSION
-./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Dpmd.version=$VERSION
+./mvnw clean package -Dpmd.dist.bin.baseDirectory=pmd -Depmd.version=$VERSION
 rm -rf ../bin/pmd/*
-bsdtar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
+# tar --strip-components=1 -xvf target/pmd.zip -C ../bin/pmd
+unzip target/pmd.zip && mv pmd ../bin
