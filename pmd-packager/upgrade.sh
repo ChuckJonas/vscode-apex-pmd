680e5a6655edf24d0fa514f6d4b3c6624e7226c7
Andreas Dangel <andreas.dangel@pmd-code.org>
2024-09-27 19:47:03 +0200

    Improve upgrade.sh script

--- pmd-packager/upgrade.sh
+++ pmd-packager/upgrade.sh
@@ -3,12 +3,24 @@
 # exit with error on the first failing command
 set -e
 
-VERSION="$1"
-if [ -z "$VERSION" ]; then
-    VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output ".tag_name" | sed 's:.*/::')
+if [ "$1" = "latest" ]; then
+    VERSION=$(curl -s https://api.github.com/repos/pmd/pmd/releases/latest | jq --raw-output '.tag_name | sub("pmd_releases/"; "")')
+elif [ -z "$1" ] && [ -n "$npm_package_config_pmdVersion" ]; then
+    # shellcheck disable=SC2154
+    # npm provides the package.json as environment variables, but in lowercase
+    VERSION="$npm_package_config_pmdVersion"
+elif [ -n "$1" ]; then
+    # using explicit VERSION
+    VERSION="$1"
+else
+    echo "$0 [latest|7.6.0|....]"
+    exit 1
 fi
 
-TARGETDIR="../bin/pmd"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$BASEDIR"
+
+TARGETDIR="$BASEDIR/../bin/pmd"
 echo "Creating custom PMD $VERSION package in '$TARGETDIR' ..."
 
 if [ -e "$TARGETDIR/VERSION" ]; then
